016f2ec0400b95e044c7fcd4a953af10ff4b69dd
oleggy-gvz <oleggy.gvz@gmail.com>
2019-07-02 17:01:07 +0700

    up

--- main.cpp
+++ main.cpp
@@ -32,9 +32,7 @@ public:
     double getY() { return Y; }
     double getZ() { return Z; }
 
-    void setX(double _X) { X = _X; }
-    void setY(double _Y) { Y = _Y; }
-    void setZ(double _Z) { Z = _Z; }
+    void setPoint(double _X, double _Y, double _Z) { X = _X; Y = _Y; Z = _Z; }
 
     friend ostream & operator<<(ostream &, const Vector3D &);
 };
@@ -54,12 +52,38 @@ private:
     Vector3D end;
 
     // line equation as intersection of two surface
-    double a1, b1, c1, d1; // a1*x + b1*y + c1*z + d1 = 0 - surface №1
-    double a2, b2, c2, d2; // a2*x + b2*y + c2*z + d2 = 0 - surface №2
+    // surface equation: a*x + b*y + c*z + d = 0
+    struct surface_ratios
+    {
+         double a, b, c, d;
+    };
+    surface_ratios surf[1]; // coefficients of two surfaces
+    double det_M2x2(double a11, double a12, double a21, double a22) { return a11 * a22 - a21 * a12; }
+
+    surface_ratios calculateRatios(Vector3D first, Vector3D second, Vector3D third)
+    {
+        surface_ratios s;
+        s.a = det_M2x2(second.getY() - first.getY(), third.getY() - start.getY(),
+                      second.getZ() - first.getZ(), third.getZ() - start.getZ());
+        s.b = -det_M2x2(second.getX() - first.getX(), third.getX() - start.getX(),
+                      second.getZ() - first.getZ(), third.getZ() - start.getZ());
+        s.c = det_M2x2(second.getX() - first.getX(), third.getX() - start.getX(),
+                      second.getY() - first.getY(), third.getY() - start.getY());
+        s.d = -start.getX() * s.a + start.getY() * (-s.b) - start.getZ() * s.c;
+        return s;
+    }
+
+    void calculateRatios()
+    {
+        surf[0] = calculateRatios(start, end, {0, -1, 2});
+        //surf[1] = calculateRatios(start, end, Vector3D());
+    }
 
 public:
     Segment3D(Vector3D _start, Vector3D _end) : start(_start), end(_end)
-    {}
+    {
+        calculateRatios();
+    }
 
     friend ostream & operator<<(ostream &, const Segment3D &);
     friend Vector3D Intersect(Segment3D s1, Segment3D s2);
@@ -88,10 +112,11 @@ int main()
 
     cout << "a2 = " << a2 << endl;
 
-    Segment3D s1(a1, a2);
-    cout << "s1 = " << s1 << endl;
+    //Segment3D s1(a1, a2);
+    //cout << "s1 = " << s1 << endl;
 
-    Vector3D b1(2, 2, 0), b2(1, 4, 0);
+    //Vector3D b1(2, 2, 0), b2(1, 4, 0);
+    Vector3D b1(1, -2, 0), b2(2, 0, -1);
     Segment3D s2(b1, b2);
     cout << "s2 = " << s2 << endl;
 
