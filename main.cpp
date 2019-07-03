598f49eccb1e38b5d83dcfc3a3727157c0b837de
oleggy-gvz <oleggy.gvz@gmail.com>
2019-07-03 10:54:12 +0700

    up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <exception>
+#include <string>
 
 using namespace std;
+
+class Exception: public exception
+{
+private:
+    string m_error;
+public:
+    Exception(string error) : m_error(error) {}
+    const char* what() const noexcept { return m_error.c_str(); }
+};
 
 class Vector3D
 {
@@ -10,30 +21,19 @@ private:
     double Z;
 
 public:
-    Vector3D() : X(0), Y(0), Z(0)
-    {}
+    Vector3D() : X(0), Y(0), Z(0) {}
+    Vector3D(double _X, double _Y, double _Z) : X(_X), Y(_Y), Z(_Z) {}
 
-    Vector3D(double _X, double _Y, double _Z) : X(_X), Y(_Y), Z(_Z)
-    {}
-
-    Vector3D operator=(const Vector3D &v)
-    {
-        X = v.X; Y = v.Y; Z = v.Z;
-        return *this;
-    }
-
-    Vector3D operator+(const Vector3D &v)
-    {
-        Vector3D tmp(X+v.X, Y+v.Y, Z+v.Z);
-        return tmp;
-    }
+    Vector3D operator=(const Vector3D &v) { X = v.X; Y = v.Y; Z = v.Z; return *this; }
+    Vector3D operator+(const Vector3D &v) { Vector3D tmp(X+v.X, Y+v.Y, Z+v.Z); return tmp; }
+    Vector3D operator-(const Vector3D &v) { Vector3D tmp(X-v.X, Y-v.Y, Z-v.Z); return tmp; }
 
     double getX() { return X; }
     double getY() { return Y; }
     double getZ() { return Z; }
+    bool isNull() { return X == 0 && Y == 0 && Z == 0; }
 
-    void setPoint(double _X, double _Y, double _Z) { X = _X; Y = _Y; Z = _Z; }
-
+    //void setPoint(double _X, double _Y, double _Z) { X = _X; Y = _Y; Z = _Z; }
     friend ostream & operator<<(ostream &, const Vector3D &);
 };
 
@@ -51,32 +51,36 @@ private:
     Vector3D start;
     Vector3D end;
 
-    // line equation as intersection of two surface
-    // surface equation: a*x + b*y + c*z + d = 0
-    struct surface_ratios
-    {
-         double a, b, c, d;
-    };
-    surface_ratios surf[1]; // coefficients of two surfaces
-    double det_M2x2(double a11, double a12, double a21, double a22) { return a11 * a22 - a21 * a12; }
+    Vector3D direction;
+    struct coeff_equation  { double a, b, c, d; }; // equation coefficients: a*x + b*y + c*z + d = 0
+    coeff_equation line; // line equation
+    coeff_equation surf1, surf2; // two surface equation, alternative line equation as intersection of two surface
 
-    surface_ratios calculateRatios(Vector3D first, Vector3D second, Vector3D third)
+    double det_M2x2(double a11, double a12, double a21, double a22) { return a11 * a22 - a21 * a12; }
+    //double det_M2x2(double a[2][2]) { return a[0][0] * a[1][1] - a[1][0] * a[0][1]; }
+    double det_M2x2(double a[4]) { return a[0] * a[3] - a[2] * a[1]; }
+
+    coeff_equation calculateSurfaceRatios(Vector3D first, Vector3D second, Vector3D third) // calculation of equation surface by three points
     {
-        surface_ratios s;
-        s.a = det_M2x2(second.getY() - first.getY(), third.getY() - start.getY(),
-                      second.getZ() - first.getZ(), third.getZ() - start.getZ());
-        s.b = -det_M2x2(second.getX() - first.getX(), third.getX() - start.getX(),
-                      second.getZ() - first.getZ(), third.getZ() - start.getZ());
-        s.c = det_M2x2(second.getX() - first.getX(), third.getX() - start.getX(),
-                      second.getY() - first.getY(), third.getY() - start.getY());
-        s.d = -start.getX() * s.a + start.getY() * (-s.b) - start.getZ() * s.c;
+        // TODO: проверка на коллинеарность
+        coeff_equation s;
+        Vector3D vec1 = second - first;
+        Vector3D vec2 = third - first;
+
+        s.a =  det_M2x2(vec1.getY(), vec2.getY(), vec1.getZ(), vec2.getZ()); // for X
+        s.b = -det_M2x2(vec1.getX(), vec2.getX(), vec1.getZ(), vec2.getZ()); // for Y
+        s.c =  det_M2x2(vec1.getX(), vec2.getX(), vec1.getY(), vec2.getY()); // for Z
+        s.d = -first.getX() * s.a + first.getY() * (-s.b) - first.getZ() * s.c;
         return s;
     }
 
     void calculateRatios()
     {
-        surf[0] = calculateRatios(start, end, {0, -1, 2});
-        //surf[1] = calculateRatios(start, end, Vector3D());
+        direction = end - start;
+        if (direction.isNull()) throw Exception("points do not have to be equal");
+
+        surf1 = calculateSurfaceRatios(start, end, {0, -1, 2});
+        //surf2 = calculateSurfaceRatios(start, end, Vector3D());
     }
 
 public:
@@ -85,11 +89,11 @@ public:
         calculateRatios();
     }
 
-    friend ostream & operator<<(ostream &, const Segment3D &);
+    friend ostream& operator<<(ostream &, const Segment3D &);
     friend Vector3D Intersect(Segment3D s1, Segment3D s2);
 };
 
-ostream & operator<<(ostream &out, const Segment3D &s)
+ostream& operator<<(ostream &out, const Segment3D &s)
 {
     out << s.start << " -> " << s.end;
     return out;
@@ -105,19 +109,7 @@ Vector3D Intersect(Segment3D s1, Segment3D s2)
 
 int main()
 {
-    Vector3D v(1, 2, 0);
-    Vector3D a1(v), a2;
-    a2 = v;
-    a2 = a1 + a2;
-
-    cout << "a2 = " << a2 << endl;
-
-    //Segment3D s1(a1, a2);
-    //cout << "s1 = " << s1 << endl;
-
-    //Vector3D b1(2, 2, 0), b2(1, 4, 0);
-    Vector3D b1(1, -2, 0), b2(2, 0, -1);
-    Segment3D s2(b1, b2);
+    Segment3D s2({1, -2, 0}, {2, 0, -1});
     cout << "s2 = " << s2 << endl;
 
     return 0;
